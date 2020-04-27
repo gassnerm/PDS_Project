8d33f826ab07c8a4edf784f864e0e202c6e04720
gassnerm <gassnerm@smail.uni-koeln.de>
2020-04-27 23:57:34 +0200

    Update the setup -

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='PDS_Project',
     version='0.0.1dev1',
-    description="Semester Project - Programming Data Science",
+    description="Semester Project -- Programming Data Science",
     author="Student",
     author_email="student@uni-koeln.de",
     packages=["nextbike"],
