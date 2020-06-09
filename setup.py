2c88c086511b3d64294abae5a93a822a2e37226b
sss <gassnerm@smail.uni-koeln.de>
2020-06-09 22:40:44 +0200

    non developer mode enable

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='PDS_Project_nextbike',
-    version='0.0.1dev1',
+    version='0.0.2',
     description="Semester Project -- Programming Data Science",
     author="Student",
     author_email="gassnerm@smail.uni-koeln.de",
-    packages=["PDS_Project_nextbike"],
+    packages=['PDS_Project_nextbike', 'PDS_Project_nextbike.io', 'PDS_Project_nextbike.model'],
     install_requires=['pandas', 'scikit-learn', 'click', 'numpy', 'matplotlib', 'tensorflow','keras', 'geopy','folium~= 0.5.0'],
     entry_points={
         'console_scripts': ['PDS_Project_nextbike=PDS_Project_nextbike.cli:main']
